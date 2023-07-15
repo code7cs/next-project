7f9039b04760fafe9b41bceb16a159e33132da88
code7cs <code7cs@gmail.com>
2023-07-15 00:08:41 -0400

    feat: add metadata description

--- nextjs-blog/pages/_document.tsx
+++ nextjs-blog/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="description"
+          content="Eastern Spa Rio Grande provides high quality Chinese massage therapy in Rio Grande, NJ.
+              Our expert therapists are trained in numerous therapies including
+              Swedish and deep tissue massages, reflexology, and more."
+        ></meta>
         <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
