efad86745ed25c0fafa9f2e5564df0c1dd1c429e
code7cs <code7cs@gmail.com>
2023-07-15 21:31:57 -0400

    fix: move title to _app.tsx

--- nextjs-blog/pages/_app.tsx
+++ nextjs-blog/pages/_app.tsx
@@ -1,12 +1,20 @@
-import type { AppProps } from 'next/app'
- 
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
 /**
- * 
- * @param Component is the active page, so whenever you navigate between routes, Component will change to the new page. 
+ *
+ * @param Component is the active page, so whenever you navigate between routes, Component will change to the new page.
  *                  Therefore, any props you send to Component will be received by the page.
  * @param pageProps is an object with the initial props that were preloaded for your page by one of our data fetching methods, otherwise it's an empty object.
- * @returns 
+ * @returns
  */
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Eastern Spa Rio Grande</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
