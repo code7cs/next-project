d59c8698535a670b59b877e494b6c5b498c5d778
code7cs <code7cs@gmail.com>
2023-07-28 17:39:59 -0400

    feat: get userAgent

--- nextjs-blog/components/ContactUsForm.tsx
+++ nextjs-blog/components/ContactUsForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ToastMessage from "./ToastMessage";
 
 const initialFormData = {
@@ -11,6 +11,18 @@ const initialFormData = {
 const ContactUsForm = () => {
   const [formData, setFormData] = useState(initialFormData);
   const [showToast, setShowToast] = useState(false);
+
+  const [userAgent, setUserAgent] = useState("");
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const device = getDevice(userAgent);
+    const browser = getBrowser(userAgent);
+
+    setUserAgent(
+      userAgent + ". Device is: " + device + ". Browser is: " + browser
+    );
+  }, []);
 
   const handleShowToast = () => {
     setShowToast(true);
@@ -53,6 +65,11 @@ const ContactUsForm = () => {
       <tr>
         <th>Message:</th>
         <td>${message}</td>
+      </tr>
+
+      <tr>
+        <th>User Agent:</th>
+        <td>${userAgent}}</td>
       </tr>
     </table>
 `;
@@ -154,3 +171,43 @@ const ContactUsForm = () => {
 };
 
 export default ContactUsForm;
+
+// Helper function to get device information
+function getDevice(userAgent) {
+  if (userAgent.match(/iPhone/i)) {
+    return "iPhone";
+  } else if (userAgent.match(/Android/i)) {
+    return "Android";
+  } else if (userAgent.match(/iPad/i)) {
+    return "iPad";
+  } else if (userAgent.match(/Windows Phone/i)) {
+    return "Windows Phone";
+  } else if (userAgent.match(/Windows/i)) {
+    return "Windows PC";
+  } else if (userAgent.match(/Macintosh/i)) {
+    return "Mac";
+  } else if (userAgent.match(/Linux/i)) {
+    return "Linux";
+  } else {
+    return "Unknown Device";
+  }
+}
+
+// Helper function to get browser information
+function getBrowser(userAgent) {
+  if (userAgent.match(/Edge/i)) {
+    return "Microsoft Edge";
+  } else if (userAgent.match(/Chrome/i)) {
+    return "Google Chrome";
+  } else if (userAgent.match(/Safari/i)) {
+    return "Safari";
+  } else if (userAgent.match(/Firefox/i)) {
+    return "Mozilla Firefox";
+  } else if (userAgent.match(/Opera|OPR/i)) {
+    return "Opera";
+  } else if (userAgent.match(/Trident/i) || userAgent.match(/MSIE/i)) {
+    return "Internet Explorer";
+  } else {
+    return "Unknown Browser";
+  }
+}
